75fd324fe10e3b54ee6b791c473d4c172945fc88
Texhnolyze <ichigobankai136@gmail.com>
2022-02-24 21:48:16 -0600

    se agrego la funcionalidad de dos botones

--- src/main/java/com/mx/pagotrajadores/PagoNomina.java
+++ src/main/java/com/mx/pagotrajadores/PagoNomina.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
@@ -16,14 +17,15 @@ public class PagoNomina extends JFrame {
 	private JTextField txtTrabajador;
 	private JTextField txtHoras;
 	private JTextField txtTarifas;
+	private JTextArea txtR;
 
 	/**
-	 * Create the frame.
+	 * se crea un frame
 	 */
 	public PagoNomina() {
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//posicion y tamaño de la ventana
+		//posicion y tamaño de la ventana 
 		setBounds(100, 100, 450, 350);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -49,9 +51,40 @@ public class PagoNomina extends JFrame {
 		contentPane.add(txtHoras);
 		txtHoras.setColumns(10);
 		
+		txtR = new JTextArea();
+		txtR.setBounds(49, 195, 331, 77);
+		contentPane.add(txtR);
+		
+		
 		JButton btnMostrar = new JButton("MOSTRAR");
 		btnMostrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//1
+				String trabajador = txtTrabajador.getText();
+				double tarifa = Double.parseDouble(txtTarifas.getText());
+				int horas = Integer.parseInt(txtHoras.getText());
+				//2
+				int extras = 0;
+				if(horas > 40) {
+					extras = horas - 40;
+					horas = 40;
+				}
+				//3
+				int excede = 0;
+				if(extras > 8) {
+					excede = extras - 8;
+					extras = 8;
+				}
+				//4
+				double monto = horas*tarifa+extras*(tarifa*2)+excede*(tarifa*3);
+				//5
+				txtR.setText("");
+				txtR.append("\nTrabajador: " + trabajador);
+				txtR.append("\nHoras de trabajo: " + horas);
+				txtR.append("\nTarifa: " + tarifa);
+				txtR.append("\nHoras Extras: " + extras);
+				txtR.append("\nHoras Extras de exceso: " + excede);
+				txtR.append("\nMonto a recibir: $ " + String.format("%.2f",monto ));
 				
 			}
 		});
@@ -63,11 +96,15 @@ public class PagoNomina extends JFrame {
 		btnNewButton_1.setBounds(227, 149, 85, 21);
 		contentPane.add(btnNewButton_1);
 		
-		JTextArea txtR = new JTextArea();
-		txtR.setBounds(49, 195, 331, 77);
-		contentPane.add(txtR);
-		
+		//valio
 		JButton btnSalir = new JButton("SALIR");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int r = JOptionPane.showOptionDialog(this, "Estas seguro de salir...?","Control de Pagos",JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null,null,null);
+				if(r == 0) System.exit(0);
+				
+			}
+		});
 		btnSalir.setBounds(49, 282, 85, 21);
 		contentPane.add(btnSalir);
 		
