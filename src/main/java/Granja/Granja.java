e92e7bca0340c925fdd7282ce49d6c28c43616f7
JosafatJimenezB <josa221001@gmail.com>
2022-02-24 21:56:45 -0600

    feat: update del nombre del archivo Granja2 a Granja

--- src/main/java/Granja/Granja.java
+++ src/main/java/Granja/Granja.java
@@ -10,12 +10,12 @@ import javax.swing.JOptionPane;
  *
  * @author golos
  */
-public class Granja2 extends javax.swing.JFrame {
+public class Granja extends javax.swing.JFrame {
 
     /**
      * Creates new form Granja2
      */
-    public Granja2() {
+    public Granja() {
         initComponents();
         Limpiar();
     }
@@ -212,20 +212,21 @@ public class Granja2 extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Granja2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Granja.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Granja2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Granja.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Granja2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Granja.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Granja2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Granja.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Granja2().setVisible(true);
+                new Granja().setVisible(true);
             }
         });
     }
